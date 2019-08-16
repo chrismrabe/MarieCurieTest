a8fd08dddff0f0e94c3a73dd5a42f258f5da8809
chris <chrismrabe@gmail.com>
2019-08-16 09:02:27 +0100

    Moved the extensions, as were display helpers, forgot to update test and remove previous service extensions.

--- HelperServiceTests/Extensions/ServiceExtensions.cs
+++ HelperServiceTests/Extensions/ServiceExtensions.cs
@@ -1,18 +1,19 @@
 ﻿using HelperServiceModels.Models;
 using HelperServices.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewTask.Helpers;
 
 namespace HelperServiceTests.Extensions
 {
     [TestClass]
-    public class ServiceExtensionsTests
+    public class ServiceHelperTests
     {
         [TestMethod]
         public void OpeningHours_NullService_ReturnEmptyString()
         {
             HelperServiceModel helperService = new HelperServiceModel();
 
-            var result = ServiceExtensions.OpeningHours(helperService);
+            var result = ServiceHelpers.OpeningHours(helperService);
             Assert.AreEqual(result, string.Empty);
 
         }
